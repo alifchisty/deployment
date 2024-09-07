7b78692f46a425b2776e12b61f9f0d147638bbe1
alifchisty <aichisty115@gmail.com>
2024-09-08 00:17:18 +0600

    finnal

--- src/app.js
+++ src/app.js
@@ -4,27 +4,28 @@ const hbs = require('hbs');
 const bcrypt = require('bcrypt');
 const User = require('./models/register');
 require('./db/conn');
+const cors = require('cors');
 
 const app = express();
 //public static path
 const static_path= path.join(__dirname, "../public");
-const template_path= path.join(__dirname, "../public/tamplates/views") 
+const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials")
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use(express.static(static_path));
+app.use(cors({ origin: 'https://el-dorados.netlify.app' }));
 
 app.set(`view engine`, `hbs`);
 app.set(`views`, template_path);
 hbs.registerPartials(partials_path);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(static_path, 'register.html'))
+  res.sendFile(path.join(static_path, 'register.html')); // Choose one to keep
 });
-app.get('/register', (req, res) => {
-  res.sendFile(path.join(static_path, 'register.html'))
-});
+
+
 
 app.post('/register', async (req, res) => {
     try {
